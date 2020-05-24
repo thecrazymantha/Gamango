5a7280eed4eb0ac37fbf949a142830bad4688e67
thecrazymantha <62051270+thecrazymantha@users.noreply.github.com>
2020-05-24 16:35:18 +0200

    conflits et erreurs résolue

--- Hebdomadaire/event.js
+++ Hebdomadaire/event.js
@@ -1,23 +1,23 @@
-let m = new Date();
-let n = m.getDay();
-let o = m.getDate();
-let p = m.getHours();
-  m.setDate(o + (7 - n) % 7);
-  m.setHours(23,59,59);
+const m = new Date();
+const n = m.getDay();
+const o = m.getDate();
+const p = m.getHours();
+m.setDate(o + (7 - n) % 7);
+m.setHours(23, 59, 59);
 
-let countDownDate = new Date(m).getTime();
+const countDownDate = new Date(m).getTime();
 
-let x = setInterval(function() {
-  let now = new Date().getTime();
-  let distance = countDownDate - now;
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+const x = setInterval(function() {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById('demo').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s `;
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "cheh, c'est trop tard";
-    document.getElementById("bouton").disabled = true; 
+    document.getElementById('demo').innerHTML = "cheh, c'est trop tard";
+    document.getElementById('bouton').disabled = true;
   }
 }, 1000);
