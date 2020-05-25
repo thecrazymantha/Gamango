4d24d408fb13e3c69e4b54fe609620c8d9c6c677
thecrazymantha <62051270+thecrazymantha@users.noreply.github.com>
2020-05-26 00:34:28 +0200

    update classement

--- import/api/Login/login.js
+++ import/api/Login/login.js
@@ -2,9 +2,9 @@
 import './login.html';
 import './login.css';
 
-//import { Accounts } from 'meteor/accounts-base';
+// import { Accounts } from 'meteor/accounts-base';
 
-//PART v.2
+// PART v.2
 /*
 Tracker.autorun(function(){
 	if(Meteor.userId()){
@@ -14,83 +14,80 @@ Tracker.autorun(function(){
 */
 //
 Template.login.rendered = function() {
-	$("#login-link").addClass('selected');
-	$("#profile-link").removeClass('selected');
-	$("#classement-link").removeClass('selected');
-	$("#map-link").removeClass('selected');
-}
+  $('#login-link').addClass('selected');
+  $('#profile-link').removeClass('selected');
+  $('#classement-link').removeClass('selected');
+  $('#map-link').removeClass('selected');
+};
 
 // Les events page login
 Template.login.events({
-	"submit .form-signin": function(event){
-		var email = trimInput(event.target.email.value);
-		var password = trimInput(event.target.password.value);
+  'submit .form-signin'(event) {
+    const email = trimInput(event.target.email.value);
+    const password = trimInput(event.target.password.value);
 
-		if(isNotEmpty(email) &&
-			isNotEmpty(password) &&
-			isEmail(email) &&
-			isValidPassword(password)){
+    if (isNotEmpty(email)
+			&& isNotEmpty(password)
+			&& isEmail(email)
+			&& isValidPassword(password)) {
+      Meteor.loginWithPassword(email, password, function(err) {
+        if (err) {
+          Bert.alert(err.reason, 'danger', 'growl-top-right');
+          return false;
+        }
+        FlowRouter.go('home');
+        Bert.alert('You are now logged in', 'success', 'growl-top-right');
+      });
+    }
 
-			Meteor.loginWithPassword(email, password, function(err){
-				if(err) {
-					Bert.alert(err.reason, "danger", "growl-top-right");
-					return false;
-				} else {
-					FlowRouter.go('home');
-					Bert.alert("You are now logged in", "success", "growl-top-right");
-				}
-			});
-
-		}
-
-		return false // Prevent Submit
-	}
+    return false; // Prevent Submit
+  },
 
 });
 
 
-//Règles de validation pour le formulaire
+// Règles de validation pour le formulaire
 
 // Trim Helper
-let trimInput = function(val){
-	return val.replace(/^\s*|\s*$/g, "");
+let trimInput = function(val) {
+  return val.replace(/^\s*|\s*$/g, '');
 };
 
-let isNotEmpty = function(value){
-	if (value && value !== ''){
-		return true;
-	}
-	Bert.alert("Please fill in all fields", "danger", "growl-top-right");
-	return false;
+let isNotEmpty = function(value) {
+  if (value && value !== '') {
+    return true;
+  }
+  Bert.alert('Please fill in all fields', 'danger', 'growl-top-right');
+  return false;
 };
 
 // Validate Email
 isEmail = function(value) {
-	let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	if(filter.test(value)) {
-		return true;
-	}
-	Bert.alert("Please use a valid email address", "danger", "growl-top-right");
-	return false;
+  const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  if (filter.test(value)) {
+    return true;
+  }
+  Bert.alert('Please use a valid email address', 'danger', 'growl-top-right');
+  return false;
 };
 
 // Check Password Field
-isValidPassword = function(password){
-	if(password.length <6) {
-		Bert.alert("Password must be at least 6 characters", "danger", "growl-top-right");
-		return false;
-	}
-	return true;
+isValidPassword = function(password) {
+  if (password.length < 6) {
+    Bert.alert('Password must be at least 6 characters', 'danger', 'growl-top-right');
+    return false;
+  }
+  return true;
 };
 
-// Match password 
+// Match password
 areValidPasswords = function(password, confirm) {
-	if(!isValidPassword(password)) {
-		return false;
-	}
-	if(password !== confirm) {
-		Bert.alert("Passwords do not match", "danger", "growl-top-right");
-		return false;
-	}
-	return true;
-}
+  if (!isValidPassword(password)) {
+    return false;
+  }
+  if (password !== confirm) {
+    Bert.alert('Passwords do not match', 'danger', 'growl-top-right');
+    return false;
+  }
+  return true;
+};
--- import/api/classement/classement.js
+++ import/api/classement/classement.js
@@ -1,4 +1,38 @@
 
+// import des fichiers js
+import '../login/login.js';
+import '../signup/signup.js';
+
 // import du CSS et du HTML
-import './classement.html';
 import './classement.css';
+import './classement.html';
+
+// Import pour les collections
+
+
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Mongo } from 'meteor/mongo';
+import { Base } from '../base.js';
+
+Template.classement.helpers({
+
+  users() {
+    /*    Meteor.users.find({}, { sort: { score: -1 } }); */
+
+    Meteor.users.find().forEach(function(oneUser) {
+      const affichage = `nom : ${oneUser.username} score : ${oneUser.profile.score}`;
+      console.log(affichage);
+      console.log(oneUser.profile.score);
+      console.log(oneUser);
+    });
+
+    return Meteor.users.find();
+  },
+
+
+});
+
+/* Meteor.users.find({}, { sort: { score: [['a', 'desc'], ['b', 'asc']] } }); */
